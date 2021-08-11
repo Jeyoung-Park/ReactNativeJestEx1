d315d63e0e41fc8b79c1bb3145781b19239ff70e
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-08-11 22:47:18 +0900

    feat: Home 페이지 추가

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
